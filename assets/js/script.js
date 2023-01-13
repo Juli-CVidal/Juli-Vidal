02a9cd8fb6c094148298b8f38b823f8b72221c9f
Juli-CVidal <juli.vidal.019@gmail.com>
2023-01-13 20:08:04 -0300

    Contact form working

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,9 +48,53 @@ let swiperProjects = new Swiper(".projects__container", {
   keyboard: true,
 });
 
-
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  contactMessage = document.getElementById("contact-message"),
+  resultMessage = document.getElementById("result-message");
 
+const sendEmail = (event) => {
+  event.preventDefault();
+  // Check that the fields are not empty
+  if (
+    [contactName.value, contactEmail.value, contactMessage.value].some(
+      (val) => val === ""
+    )
+  ) {
+    //Change color
+    resultMessage.classList.remove("color-blue");
+    resultMessage.classList.add("color-red");
+
+    //Show message
+    resultMessage.innerText = "Not all fields have been filled in";
+  } else {
+    //serviceID - templateID -#form - publicKey
+    emailjs
+      .sendForm(
+        "service_n5gksoj",
+        "template_qaxoco5",
+        "#contact-form",
+        "XVXoSmc612W-44U7o"
+      )
+      .then(() => {
+        resultMessage.classList.remove("color-red");
+        resultMessage.classList.add("color-blue");
+        resultMessage.innerText = "Message sent successfully!";
+
+        setTimeout(() => {
+          contactMessage.innerText = "";
+        
+        },5000)
+      },(error)=> {
+        alert("Something has failed :c -> ", error)
+      });
+
+      contactForm.reset();
+  }
+};
+contactForm.addEventListener("submit", sendEmail);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/

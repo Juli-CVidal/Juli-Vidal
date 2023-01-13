36538f513a850a897618da305a3ab409f5a00fa3
Juli-CVidal <juli.vidal.019@gmail.com>
2023-01-13 20:37:51 -0300

    Scrollup button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,26 +78,58 @@ const sendEmail = (event) => {
         "#contact-form",
         "XVXoSmc612W-44U7o"
       )
-      .then(() => {
-        resultMessage.classList.remove("color-red");
-        resultMessage.classList.add("color-blue");
-        resultMessage.innerText = "Message sent successfully!";
+      .then(
+        () => {
+          resultMessage.classList.remove("color-red");
+          resultMessage.classList.add("color-blue");
+          resultMessage.innerText = "Message sent successfully!";
 
-        setTimeout(() => {
-          resultMessage.innerText = "";
-        
-        },5000)
-      },(error)=> {
-        alert("Something has failed :c -> ", error)
-      });
+          setTimeout(() => {
+            resultMessage.innerText = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("Something has failed :c -> ", error);
+        }
+      );
 
-      contactForm.reset();
+    contactForm.reset();
   }
 };
 contactForm.addEventListener("submit", sendEmail);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== DARK LIGHT THEME ===============*/
 

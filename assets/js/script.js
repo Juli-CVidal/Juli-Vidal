6f08635562958a7edde9eebea8c49a5763f95b00
Juli-CVidal <juli.vidal.019@gmail.com>
2023-05-16 22:19:31 -0300

    Starting with mix-blend and background

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,6 @@ const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
@@ -51,20 +50,16 @@ const contactForm = document.getElementById("contact-form"),
 
 const sendEmail = (event) => {
   event.preventDefault();
-  // Check that the fields are not empty
   if (
     [contactName.value, contactEmail.value, contactMessage.value].some(
       (val) => val === ""
     )
   ) {
-    //Change color
+  
     resultMessage.classList.remove("color-blue");
     resultMessage.classList.add("color-red");
-
-    //Show message
     resultMessage.innerText = "Not all fields have been filled in";
   } else {
-    //serviceID - templateID -#form - publicKey
     emailjs
       .sendForm(
         "service_n5gksoj",
@@ -118,7 +113,6 @@ window.addEventListener("scroll", scrollActive);
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
   this.scrollY >= 350
     ? scrollUp.classList.add("show-scroll")
     : scrollUp.classList.remove("show-scroll");
@@ -130,19 +124,15 @@ const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "ri-sun-line";
 
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 
-// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
@@ -151,20 +141,23 @@ if (selectedTheme) {
   );
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
+
+/* ======== BACKGROUND RELATED ======== */
+const flipButton = document.getElementById("flip-button");
+flipButton.addEventListener("click", () => {
+  document.body.classList.toggle("background-visible");
+})
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
   const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
   this.scrollY >= 50
     ? header.classList.add("bg-header")
     : header.classList.remove("bg-header");
